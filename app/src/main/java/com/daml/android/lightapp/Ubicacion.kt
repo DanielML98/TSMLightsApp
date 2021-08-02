927d158b4aa3814e0ed1ef54f9926f40a077da40
Humberto <beto.gonzalez.vela@gmail.com>
2021-08-01 23:26:42 -0500

    modificando los archivos build.gradle y AndroidManfest.xml para permitir obtener ubicacion

---
+++ app/src/main/java/com/daml/android/lightapp/Ubicacion.kt
@@ -0,0 +1,11 @@
+package com.daml.android.lightapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Ubicacion : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ubicacion)
+    }
+}
