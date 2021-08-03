f4111beca90c68d7a02290d02668d2937cd607c0
fjdurlop <fjdurlop0@gmail.com>
2021-08-02 21:12:28 -0500

    Updating urls and changing status according to server status

--- app/src/main/java/com/daml/android/lightapp/LightsActivity.kt
+++ app/src/main/java/com/daml/android/lightapp/LightsActivity.kt
@@ -21,6 +21,7 @@ class LightsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lights)
+        initialUpdateStatus()
     }
 
     //This is the onClick listener for the three light buttons
@@ -52,7 +53,7 @@ class LightsActivity : AppCompatActivity() {
 
 
     // This function changes label text to the returned string in the GET request to the url
-    fun getFromUrl(url: String,label: TextView){
+    fun getFromUrl(url: String,label: TextView,id:Int){
 
         val label = label
         //val label : TextView = findViewById(R.id.label)
@@ -64,9 +65,31 @@ class LightsActivity : AppCompatActivity() {
             Request.Method.GET, url,
             Response.Listener<String> { response ->
                 // Display the first 500 characters of the response string.
-                val strResp = response.toString()
-                //println(strResp)
+                var strResp = response.toString()
+
+                if(id ==1){
+                    strResp = strResp.substring(9,10)
+                    bulbOneStatus = true
+                }
+                else if(id ==2){
+                    strResp = strResp.substring(22,23)
+                    bulbTwoStatus = true
+                }
+                else if(id ==3){
+                    strResp = strResp.substring(35,36)
+                    bulbThreeStatus = true
+                }
+                else if(id ==4){
+                    strResp = strResp.substring(48,49)
+                    bulbFourStatus= true
+                }
+
+                println(strResp)
                 label.text = strResp
+                println(bulbOneStatus)
+                println(bulbTwoStatus)
+                println(bulbThreeStatus)
+                println(bulbFourStatus)
 
             },
             Response.ErrorListener { println("Error") })
@@ -77,26 +100,29 @@ class LightsActivity : AppCompatActivity() {
     }
 
     // Function to get the initial status
-    fun initialUpdateStatus(view: View){
+    fun initialUpdateStatus(){
 
         // The status obtained will be in the value of labels.text
-        //val label_1 : TextView = findViewById(R.id.label2)
-        //val label_2 : TextView = findViewById(R.id.label3)
-        //val label_3 : TextView = findViewById(R.id.label4)
-        //val label_4 : TextView = findViewById(R.id.label5)
+        val label_1 : TextView = findViewById(R.id.textView1)
+        val label_2 : TextView = findViewById(R.id.textView2)
+        val label_3 : TextView = findViewById(R.id.textView3)
+        val label_4 : TextView = findViewById(R.id.textView4)
 
         // Change to the url to obtain status of each bulb
-        val url_get_satus_1 = "https://tsmpjgv9.000webhostapp.com/get.php?status=0&ID=1" //url for bulb_1
-        val url_get_satus_2 = "https://tsmpjgv9.000webhostapp.com/get.php?status=0&ID=2"
-        val url_get_satus_3 = "https://tsmpjgv9.000webhostapp.com/get.php?status=0&ID=1"
-        val url_get_satus_4 = "https://tsmpjgv9.000webhostapp.com/get.php?status=0&ID=2"
+        val url_get_satus_1 = "https://appdevops.000webhostapp.com/ConsultaESP32.php" //url for bulb_1
+        val url_get_satus_2 = "https://appdevops.000webhostapp.com/ConsultaESP32.php"
+        val url_get_satus_3 = "https://appdevops.000webhostapp.com/ConsultaESP32.php"
+        val url_get_satus_4 = "https://appdevops.000webhostapp.com/ConsultaESP32.php"
 
         // For each bulb use a label
-        //getFromUrl(url_get_satus_1,label_1)
-        //getFromUrl(url_get_satus_2,label_2)
-        //getFromUrl(url_get_satus_3,label_3)
-        //getFromUrl(url_get_satus_4,label_4)
+        getFromUrl(url_get_satus_1,label_1,1)
+        getFromUrl(url_get_satus_2,label_2,2)
+        getFromUrl(url_get_satus_3,label_3,3)
+        getFromUrl(url_get_satus_4,label_4,4)
 
+        println(bulbOneStatus)
+        println(bulbTwoStatus)
+        println(bulbThreeStatus)
 
     }
 
