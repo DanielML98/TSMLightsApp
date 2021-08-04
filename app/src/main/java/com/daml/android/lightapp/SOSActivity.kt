41d044e10c1eecb9032bce23bf06d6ca0c63f378
Athenas-Marlene-GM <athenas97@comunidad.unam.mx>
2021-08-04 16:34:40 -0500

    Nueva actividad para llamada SOS

---
+++ app/src/main/java/com/daml/android/lightapp/SOSActivity.kt
@@ -0,0 +1,11 @@
+package com.daml.android.lightapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SOSActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_s_o_s)
+    }
+}
