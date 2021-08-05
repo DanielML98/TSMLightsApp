f1cd931dbcb47afba175e21a977e6daf1e5847b0
Athenas-Marlene-GM <athenas97@comunidad.unam.mx>
2021-08-04 19:01:58 -0500

    Se envia un mensaje cualquiera al presionar el botón

--- app/src/main/java/com/daml/android/lightapp/SOSActivity.kt
+++ app/src/main/java/com/daml/android/lightapp/SOSActivity.kt
@@ -2,10 +2,23 @@ package com.daml.android.lightapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.telephony.SmsManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 class SOSActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_s_o_s)
+        val button: Button = findViewById(R.id.btnEnviarSMS)
+        button.setOnClickListener {
+            var enviarSMS=SmsManager.getDefault()
+            enviarSMS.sendTextMessage("5559462779",null,"Ubicación",null,null)
+        }
+
     }
 }
