07d198c4d05737c9a2448c76ee7654584d508da2
Athenas-Marlene-GM <athenas97@comunidad.unam.mx>
2021-08-04 23:14:57 -0500

    solicitud de permisos al usuario

--- app/src/main/java/com/daml/android/lightapp/SOSActivity.kt
+++ app/src/main/java/com/daml/android/lightapp/SOSActivity.kt
@@ -8,6 +8,8 @@ import android.telephony.SmsManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 class SOSActivity : AppCompatActivity() {
 
@@ -16,9 +18,48 @@ class SOSActivity : AppCompatActivity() {
         setContentView(R.layout.activity_s_o_s)
         val button: Button = findViewById(R.id.btnEnviarSMS)
         button.setOnClickListener {
-            var enviarSMS=SmsManager.getDefault()
-            enviarSMS.sendTextMessage("5559462779",null,"Ubicación",null,null)
+            checkPermissions()
         }
 
+    }
+    private fun checkPermissions() {
+        if(ContextCompat.checkSelfPermission(this,Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED){
+            requestSendSMSPermissions()
+        }else{
+            sendSMS()
+        }
+    }
+
+    private fun requestSendSMSPermissions() {
+        if(ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.SEND_SMS)){
+            Toast.makeText(this,"Los permisos ya han sido rechazados",Toast.LENGTH_SHORT).show()
+
+        }else{
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.SEND_SMS),1)
+        }
+
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode == 1){
+            if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                sendSMS()
+            }else{
+                Toast.makeText(this,"Permisos rechazados",Toast.LENGTH_SHORT).show()
+            }
+
+        }
+    }
+
+    private fun sendSMS() {
+        var enviarSMS=SmsManager.getDefault()
+        enviarSMS.sendTextMessage("5559462779",null,"Ubicación",null,null)
+        enviarSMS.sendTextMessage("5514919708",null,"Ubicación",null,null)
+        Toast.makeText(this,"Mensaje enviado",Toast.LENGTH_SHORT).show()
     }
 }
