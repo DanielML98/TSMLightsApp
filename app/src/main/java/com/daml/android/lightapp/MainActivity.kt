e89133f1924bae9711576a3b333595b50f89fc17
Dilii <dilanemanuel@hotmail.es>
2021-08-08 03:31:38 -0500

    Sending location with SMS, added location function for testing

--- app/src/main/java/com/daml/android/lightapp/MainActivity.kt
+++ app/src/main/java/com/daml/android/lightapp/MainActivity.kt
@@ -4,15 +4,19 @@ import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.hardware.camera2.CameraManager
+import android.location.Location
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Looper
 import android.telephony.SmsManager
 import android.view.View
 import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import com.google.android.gms.location.*
+
 /*import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.StringRequest
@@ -21,14 +25,16 @@ import android.content.Context*/
 
 
 
-
-
-
 class MainActivity : AppCompatActivity() {
     private lateinit var cameraM: CameraManager
+    lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    val objetoSOSUbicacion = SOSClass(this)
+    var latitud: String = ""
+    var longitud: String = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
     }
 
     //Function for the 3 ImageButtons in activity_main.xml
@@ -54,7 +60,7 @@ class MainActivity : AppCompatActivity() {
         if(ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED){
             requestSendSMSPermissions()
         }else{
-            sendSMS()
+            getLastLocation()
         }
         if(ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
             requestCameraPermissions()
@@ -63,7 +69,7 @@ class MainActivity : AppCompatActivity() {
                 flashLight()
             }
         }
-
+        objetoSOSUbicacion.RequestPermissionLocation()
     }
 
     @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
@@ -74,6 +80,8 @@ class MainActivity : AppCompatActivity() {
             val  cameraListId = cameraM.cameraIdList[0]
             cameraM.setTorchMode(cameraListId,true)
         }
+
+        Toast.makeText(this,"Linterna encendida",Toast.LENGTH_SHORT).show()
     }
 
     private fun requestCameraPermissions() {
@@ -121,25 +129,72 @@ class MainActivity : AppCompatActivity() {
 
     private fun sendSMS() {
         val enviarSMS= SmsManager.getDefault()
-        val numeroUno=5574044562
+        val numeroUno=5571959003
         val numeroDos=5548698086
 
         //Formato para enviar mensaje SOS
-        val enlaceUbicacion ="https://www.google.com.mx/maps/preview"
-        val latitud="19.809045"
-        val longitud="-98.9045"
-        val mensaje =
-            "Eres mi contacto de emergencia, ¡ayuda!, esta es mi ubicacion:\n\n$enlaceUbicacion\n\nLatitud: $latitud\nLongitud: $longitud"
-        println(mensaje)
+        val enlaceUbicacion ="https://www.google.com.mx/maps/search/?api=1&query"
 
-        //Sentencia para enviar mensaje a contacto
-        enviarSMS.sendTextMessage("+$numeroUno.toString()",null,
-            "SOS, está es mi ubicación: $enlaceUbicacion",null,null)
-        enviarSMS.sendTextMessage("+$numeroDos.toString()",null,
-            mensaje,null,null)
+        if(latitud != "" && longitud != ""){
+            val mensaje =
+                "Eres mi contacto de emergencia, ¡ayuda!, esta es mi ubicacion:\n\n$enlaceUbicacion$latitud,$longitud\n\nLatitud: $latitud\nLongitud: $longitud"
+            println(mensaje)
 
-        //Mensaje en la aplicación de éxito
-        Toast.makeText(this,"Mensaje enviado a tus dos contactos",Toast.LENGTH_SHORT).show()
-        Toast.makeText(this,"Linterna encendida",Toast.LENGTH_SHORT).show()
+            //Sentencia para enviar mensaje a contacto
+            enviarSMS.sendTextMessage("$numeroUno",null,
+                "SOS, esta es mi ubicacion: $enlaceUbicacion$latitud,$longitud",null,null)
+            //enviarSMS.sendTextMessage("+$numeroDos.toString()",null,
+            //mensaje,null,null)
+
+            //Mensaje en la aplicación de éxito
+            Toast.makeText(this,"Mensaje enviado a tus dos contactos",Toast.LENGTH_SHORT).show()
+        }else{
+            Toast.makeText(this,"Hubo un error, revisa permisos de ubicación y que tengas la ubicación activada",Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    fun getLastLocation(){
+        if(objetoSOSUbicacion.checkPermission()){
+            if(objetoSOSUbicacion.isLocationEnable()) {
+                fusedLocationProviderClient.lastLocation.addOnCompleteListener { task ->
+                    var location: Location? = task.result
+                    if (location == null) {
+                        var locationRequest = LocationRequest()
+                        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+                        locationRequest.interval = 0
+                        locationRequest.fastestInterval = 0
+                        locationRequest.numUpdates = 1
+                        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+                        fusedLocationProviderClient!!.requestLocationUpdates(
+                            locationRequest, locationCallback, Looper.myLooper()
+                        )
+                    } else {
+                        //Usar location.longitude y location.latitude
+                        latitud = location.latitude.toString()
+                        longitud = location.longitude.toString()
+                        //Para pruebas directas
+                        //Toast.makeText(contextM, "get ${location.latitude}", Toast.LENGTH_SHORT).show()
+                        //Toast.makeText(contextM, "get ${location.longitude}", Toast.LENGTH_SHORT).show()
+                    }
+                    sendSMS()
+                }
+            }else{
+                Toast.makeText(this, "Activa ubicación en tu teléfono", Toast.LENGTH_SHORT).show()
+            }
+        }else{
+            objetoSOSUbicacion.RequestPermissionLocation()
+        }
+    }
+
+    private val locationCallback = object : LocationCallback(){
+        override fun onLocationResult(locationResult: LocationResult){
+            val lastLocation: Location = locationResult.lastLocation
+            //Usar lastLocation.longitude y lastLocation.latitude
+            latitud = lastLocation.latitude.toString()
+            longitud = lastLocation.longitude.toString()
+            //Para pruebas directas
+            //Toast.makeText(contextM, "loc"+lastLocation.latitude.toString(), Toast.LENGTH_SHORT).show()
+            //Toast.makeText(contextM, "loc"+lastLocation.longitude.toString(), Toast.LENGTH_SHORT).show()
+        }
     }
 }
---
+++ app/src/main/java/com/daml/android/lightapp/SOSClass.kt
@@ -0,0 +1,39 @@
+package com.daml.android.lightapp
+
+import android.app.Activity
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.LocationManager
+import androidx.core.app.ActivityCompat
+
+class SOSClass (context: Activity){
+    var contextM = context
+    val PERMISSION_ID : Int = 28
+
+    //Clase encargada de solicitar permisos de ubicación para SOS, se podrá editar o quitar luego
+
+    //Función que revisa si a la app se le han dado permisos para ubicación
+    fun checkPermission(): Boolean {
+        if(ActivityCompat.checkSelfPermission(contextM, android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
+            ||ActivityCompat.checkSelfPermission(contextM, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            return true
+        }
+        return false
+    }
+
+    //Solicita permiso de ubicación al usuario si no se ha otorgado
+    fun RequestPermissionLocation(){
+        ActivityCompat.requestPermissions(
+            contextM, arrayOf(android.Manifest.permission.ACCESS_COARSE_LOCATION, android.Manifest.permission.ACCESS_FINE_LOCATION),
+            PERMISSION_ID
+        )
+    }
+
+    //Checa si el usuario activó ubicación
+    fun isLocationEnable():Boolean{
+        var locationManager = contextM.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) ||
+                locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+    }
+
+}
